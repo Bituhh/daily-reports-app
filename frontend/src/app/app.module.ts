2d6e0e3bb2db332bac1313462bdd2352ac7e0f1c
Victor Oliveira <victor@oliveira.org.uk>
2021-01-26 22:22:54 +0000

    minor import issue

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,15 +1,16 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {RouteReuseStrategy} from '@angular/router';
-import {IonicModule, IonicRouteStrategy} from '@ionic/angular';
+import {metaReducers, reducers} from '@core/store';
+import {environment} from '@env/environment';
 import {SplashScreen} from '@ionic-native/splash-screen/ngx';
 import {StatusBar} from '@ionic-native/status-bar/ngx';
-import {AppComponent} from './app.component';
-import {AppRoutingModule} from './app-routing.module';
+import {IonicModule, IonicRouteStrategy} from '@ionic/angular';
+import {EffectsModule} from '@ngrx/effects';
 import {StoreModule} from '@ngrx/store';
 import {StoreDevtoolsModule} from '@ngrx/store-devtools';
-import {environment} from '@env/environment';
-import {EffectsModule} from '@ngrx/effects';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
@@ -18,9 +19,7 @@ import {EffectsModule} from '@ngrx/effects';
     BrowserModule,
     IonicModule.forRoot(),
     AppRoutingModule,
-    StoreModule.forRoot(reducers, {
-      metaReducers,
-    }),
+    StoreModule.forRoot(reducers, {metaReducers}),
     StoreDevtoolsModule.instrument({maxAge: 25, logOnly: environment.production}),
     EffectsModule.forRoot([]),
   ],
